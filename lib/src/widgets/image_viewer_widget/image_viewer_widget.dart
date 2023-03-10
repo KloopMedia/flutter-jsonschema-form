3aa2eedd0eaea364920548d1759c89f10ab20238
ramil.salihar <ramil.salihar_2025@ucentralasia.org>
2023-03-10 19:57:38 +0600

    <fix> Creating closing button on image_viewer_widget

--- lib/src/widgets/image_viewer_widget/image_viewer_widget.dart
+++ lib/src/widgets/image_viewer_widget/image_viewer_widget.dart
@@ -8,30 +8,46 @@ class ImageViewerWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CachedNetworkImage(
-      imageUrl: url,
-      progressIndicatorBuilder: (context, url, downloadProgress) {
-        return CircularProgressIndicator(value: downloadProgress.progress);
-      },
-      errorWidget: (context, url, error) {
-        print(error);
-        return Dialog(
-          child: SizedBox(
-            width: 150,
-            height: 150,
-            child: Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: const [
-                  Icon(Icons.error),
-                  SizedBox(height: 12),
-                  Text('Error: Failed to load the image!'),
-                ],
-              ),
-            ),
+    return Stack(children: [
+        Container(
+          alignment: AlignmentDirectional.center,
+          child: CachedNetworkImage(
+            imageUrl: url,
+            progressIndicatorBuilder: (context, url, downloadProgress) {
+              return CircularProgressIndicator(value: downloadProgress.progress);
+            },
+            errorWidget: (context, url, error) {
+              print(error);
+              return Dialog(
+                child: SizedBox(
+                  width: 150,
+                  height: 150,
+                  child: Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: const [
+                        Icon(Icons.error),
+                        SizedBox(height: 12),
+                        Text('Error: Failed to load the image!'),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
           ),
-        );
-      },
-    );
+        ),
+      Align(
+        alignment: Alignment.topRight,
+        child: Material(
+          child: IconButton(
+            icon: const Icon(Icons.close_outlined),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+      ),
+      ]);
   }
 }
