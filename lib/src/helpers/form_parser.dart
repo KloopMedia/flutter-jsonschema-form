99b776d5a9792342bfe48d3c0ee455063d74d9d6
atna <atay.narynov@gmail.com>
2023-10-18 13:00:23 +0600

    Remove class.

--- lib/src/helpers/form_parser.dart
+++ lib/src/helpers/form_parser.dart
@@ -155,181 +155,169 @@ class DynamicArray extends ArrayField {
         );
 }
 
-class SchemaParser {
-  final Map<String, dynamic> schema;
-  final Map<String, dynamic>? uiSchema;
-  final Map<String, dynamic> formData;
+FieldType getFieldType(Map<String, dynamic> schema) {
+  final maybeObject = schema.containsKey("properties") ? "object" : null;
+  return decodeFieldType(schema['type'] ?? maybeObject);
+}
 
-  const SchemaParser({
-    required this.schema,
-    this.uiSchema,
-    required this.formData,
-  });
+List<String> _getRequiredFields(Map<String, dynamic> schema) {
+  if (schema.containsKey('required')) {
+    return (schema['required'] as List<dynamic>).cast();
+  } else {
+    return [];
+  }
+}
 
-  static FieldType getFieldType(Map<String, dynamic> schema) {
-    final maybeObject = schema.containsKey("properties") ? "object" : null;
-    return decodeFieldType(schema['type'] ?? maybeObject);
+List<Field> _parseObjectFields(
+  Map<String, dynamic> properties,
+  List<String> required,
+  PathModel path, {
+  Map<String, dynamic>? uiSchema,
+  PathModel? dependencyParentPath,
+  List<dynamic>? dependencyConditions,
+}) {
+  final List<Field> subFields = [];
+
+  for (final entry in properties.entries) {
+    final id = entry.key;
+    final subSchema = entry.value;
+
+    final subField = parseSchema(
+      id: id,
+      schema: subSchema,
+      path: path,
+      dependencyConditions: dependencyConditions,
+      dependencyParentPath: dependencyParentPath,
+      isRequired: required.contains(id),
+      uiSchema: uiSchema?[id],
+    );
+    subFields.addAll(subField);
   }
 
-  List<String> _getRequiredFields(Map<String, dynamic> schema) {
-    if (schema.containsKey('required')) {
-      return (schema['required'] as List<dynamic>).cast();
-    } else {
-      return [];
-    }
-  }
+  return subFields;
+}
 
-  List<Field> _parseObjectFields(
-    Map<String, dynamic> properties,
-    List<String> required,
-    PathModel path, {
-    Map<String, dynamic>? uiSchema,
-    PathModel? dependencyParentPath,
-    List<dynamic>? dependencyConditions,
-  }) {
-    final List<Field> subFields = [];
+List<Field> _parseDependencyFields(
+  Map<String, dynamic> dependencies,
+  PathModel path, {
+  Map<String, dynamic>? uiSchema,
+}) {
+  final List<Field> subFields = [];
 
-    for (final entry in properties.entries) {
-      final id = entry.key;
-      final subSchema = entry.value;
-
-      final subField = parseSchema(
-        id: id,
-        schema: subSchema,
-        path: path,
-        dependencyConditions: dependencyConditions,
-        dependencyParentPath: dependencyParentPath,
-        isRequired: required.contains(id),
-        uiSchema: uiSchema?[id],
-      );
-      subFields.addAll(subField);
-    }
-
-    return subFields;
-  }
-
-  List<Field> _parseDependencyFields(
-    Map<String, dynamic> dependencies,
-    PathModel path, {
-    Map<String, dynamic>? uiSchema,
-  }) {
-    final List<Field> subFields = [];
-
-    for (final entry in dependencies.entries) {
-      final List<Map<String, dynamic>> variants = entry.value['oneOf'];
-      for (final variant in variants) {
-        final Map<String, dynamic> copy = Map.of(variant['properties']);
-        final List<dynamic> conditions = copy.remove(entry.key)?['enum'] ?? [];
-        final parentPath = path.add(entry.key, FieldType.object);
-        final fullCopy = {...variant, "properties": copy};
-        if (copy.isNotEmpty) {
-          final depFields = parseSchema(
-            schema: fullCopy,
-            path: path,
-            dependencyParentPath: parentPath,
-            dependencyConditions: conditions,
-            uiSchema: uiSchema,
-          );
-          subFields.addAll(depFields);
-        }
-      }
-    }
-
-    return subFields;
-  }
-
-  List<Field> parseSchema({
-    String id = "#",
-    required Map<String, dynamic> schema,
-    Map<String, dynamic>? uiSchema,
-    PathModel path = const PathModel.empty(),
-    PathModel? dependencyParentPath,
-    List<dynamic>? dependencyConditions,
-    bool? isRequired,
-  }) {
-    final type = getFieldType(schema);
-
-    if (type == FieldType.object) {
-      final newPath = id == "#" ? path : path.add(id, FieldType.object);
-
-      final List<String> required = _getRequiredFields(schema);
-      final objectFields = _parseObjectFields(
-        schema['properties'],
-        required,
-        newPath,
-        uiSchema: uiSchema,
-        dependencyParentPath: dependencyParentPath,
-        dependencyConditions: dependencyConditions,
-      );
-
-      List<Field> dependencyFields;
-      if (schema['dependencies'] != null) {
-        dependencyFields = _parseDependencyFields(
-          schema['dependencies'],
-          newPath,
+  for (final entry in dependencies.entries) {
+    final List<Map<String, dynamic>> variants = entry.value['oneOf'];
+    for (final variant in variants) {
+      final Map<String, dynamic> copy = Map.of(variant['properties']);
+      final List<dynamic> conditions = copy.remove(entry.key)?['enum'] ?? [];
+      final parentPath = path.add(entry.key, FieldType.object);
+      final fullCopy = {...variant, "properties": copy};
+      if (copy.isNotEmpty) {
+        final depFields = parseSchema(
+          schema: fullCopy,
+          path: path,
+          dependencyParentPath: parentPath,
+          dependencyConditions: conditions,
           uiSchema: uiSchema,
         );
-      } else {
-        dependencyFields = [];
+        subFields.addAll(depFields);
       }
-
-      final subFields = [...objectFields, ...dependencyFields];
-
-      return [
-        Section(
-          id: id,
-          path: path,
-          fields: subFields,
-          dependencyConditions: dependencyConditions,
-          dependencyParentPath: dependencyParentPath,
-        ),
-      ];
-    } else {
-      final newPath = path.add(id, FieldType.object);
-      return [
-        ValueField(
-          id: id,
-          path: newPath,
-          dependencyParentPath: dependencyParentPath,
-          dependencyConditions: dependencyConditions,
-          required: isRequired ?? false,
-          widgetType: WidgetModel.fromUiSchema(uiSchema),
-        ),
-      ];
     }
   }
 
-  List<Widget> serializeFields(List<Field> fields) {
-    final List<Widget> serializedFields = [];
+  return subFields;
+}
 
-    for (final field in fields) {
-      if (field is Section) {
-        if (field.hasTitleOrDescription) {
-          serializedFields.add(Text('${field.id} ${field.title}'));
-        }
-        if (field.isDependent) {
-          final parentValue = getFormDataByPath(formData, field.dependencyParentPath!);
-          if (field.dependencyConditions!.contains(parentValue)) {
-            serializedFields.addAll(serializeFields(field.fields));
-          }
-        } else {
-          serializedFields.addAll(serializeFields(field.fields));
-        }
-      } else if (field is FixedArray) {
+List<Field> parseSchema({
+  String id = "#",
+  required Map<String, dynamic> schema,
+  Map<String, dynamic>? uiSchema,
+  PathModel path = const PathModel.empty(),
+  PathModel? dependencyParentPath,
+  List<dynamic>? dependencyConditions,
+  bool? isRequired,
+}) {
+  final type = getFieldType(schema);
+
+  if (type == FieldType.object) {
+    final newPath = id == "#" ? path : path.add(id, FieldType.object);
+
+    final List<String> required = _getRequiredFields(schema);
+    final objectFields = _parseObjectFields(
+      schema['properties'],
+      required,
+      newPath,
+      uiSchema: uiSchema,
+      dependencyParentPath: dependencyParentPath,
+      dependencyConditions: dependencyConditions,
+    );
+
+    List<Field> dependencyFields;
+    if (schema['dependencies'] != null) {
+      dependencyFields = _parseDependencyFields(
+        schema['dependencies'],
+        newPath,
+        uiSchema: uiSchema,
+      );
+    } else {
+      dependencyFields = [];
+    }
+
+    final subFields = [...objectFields, ...dependencyFields];
+
+    return [
+      Section(
+        id: id,
+        path: path,
+        fields: subFields,
+        dependencyConditions: dependencyConditions,
+        dependencyParentPath: dependencyParentPath,
+      ),
+    ];
+  } else {
+    final newPath = path.add(id, FieldType.object);
+    return [
+      ValueField(
+        id: id,
+        path: newPath,
+        dependencyParentPath: dependencyParentPath,
+        dependencyConditions: dependencyConditions,
+        required: isRequired ?? false,
+        widgetType: WidgetModel.fromUiSchema(uiSchema),
+      ),
+    ];
+  }
+}
+
+List<Widget> serializeFields(List<Field> fields, Map<String, dynamic> formData) {
+  final List<Widget> serializedFields = [];
+
+  for (final field in fields) {
+    if (field is Section) {
+      if (field.hasTitleOrDescription) {
         serializedFields.add(Text('${field.id} ${field.title}'));
-        serializedFields.addAll(serializeFields(field.fields));
+      }
+      if (field.isDependent) {
+        final parentValue = getFormDataByPath(formData, field.dependencyParentPath!);
+        if (field.dependencyConditions!.contains(parentValue)) {
+          serializedFields.addAll(serializeFields(field.fields, formData));
+        }
       } else {
-        if (field.isDependent) {
-          final parentValue = getFormDataByPath(formData, field.dependencyParentPath!);
-          if (field.dependencyConditions!.contains(parentValue)) {
-            serializedFields.add(Text(field.id));
-          }
-        } else {
+        serializedFields.addAll(serializeFields(field.fields, formData));
+      }
+    } else if (field is FixedArray) {
+      serializedFields.add(Text('${field.id} ${field.title}'));
+      serializedFields.addAll(serializeFields(field.fields, formData));
+    } else {
+      if (field.isDependent) {
+        final parentValue = getFormDataByPath(formData, field.dependencyParentPath!);
+        if (field.dependencyConditions!.contains(parentValue)) {
           serializedFields.add(Text(field.id));
         }
+      } else {
+        serializedFields.add(Text(field.id));
       }
     }
-
-    return serializedFields;
   }
+
+  return serializedFields;
 }
